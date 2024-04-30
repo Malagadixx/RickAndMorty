bb8139622e7bc501dcf3e27df062d1003befd839
Malagadixx <henriqueemalaga@gmail.com>
2024-04-30 19:58:54 -0300

    Primeiro commit

---
+++ src/components/ListRickandMorty.vue
@@ -0,0 +1,23 @@
+<script setup>
+const rickAndMorty = defineProps(["name", "status", "species", "gender", "location", "image", "episode"])
+
+</script>
+
+<template>
+        <div class="card col-md-4">
+            <div class="card mb-3"></div>
+            <img :src="rickAndMorty.image" class="card-img-top" height="80px" alt="...">
+            <div class="card-body">
+                <div class="card-tittle text-center">
+                <p> Nome: {{ rickAndMorty.name}}</p>
+                <p> Status: {{ rickAndMorty.status}}</p>
+                <p> Espécie: {{ rickAndMorty.species}}</p>
+                <p> Gênero: {{ rickAndMorty.gender}}</p>
+                <p> Localização: {{ rickAndMorty.location }}</p>
+                <p> Episodío: {{ rickAndMorty.episode.length }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style></style>
